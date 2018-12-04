2281f28b6bc80640cff5da777f269ebe48e06901
x_tfischer <t.fischer@student.uni-tuebingen.de>
2018-12-04 14:26:58 +0100

    balanced float error in tests

--- src/tests/test_prediction.py
+++ src/tests/test_prediction.py
@@ -21,14 +21,19 @@ class TestPrediction(unittest.TestCase):
         p = PoissonPredictor()
         with self.DB.get_session() as session:
             p.calculate_model(RangeSelector(), session)
-            result = p.make_prediction("FC Bayern", "Borussia Dortmund")
-            expected_result = {'host': 0.7330641802459998, 'draw': 0.16066711952653115, 'guest': 0.10621035363363719}
+        result = {
+            key: round(val, 2)
+            for key, val in p.make_prediction(
+                "FC Bayern", "Borussia Dortmund"
+            ).items()
+        }
+        expected_result = {'host': 0.73, 'draw': 0.16, 'guest': 0.11}
 
-            assert result == expected_result, "Result wasn't what we expected..."
-            # This check incorperates all of the following:
-            # 0. assert len(result) == len(expected_result), "Result has wrong length"
-            # 1. assert len(set(result.keys()) - set(expected_result.keys())) == 0, "Unexpect result keys"
-            # 2. assert len(set(result.values()) - set(expected_result.values())) == 0, "Unexpected result values"
-            # 3. assert result["host"] == expected_result["host"], "Wrong value for host_win"
-            # 4. assert result["draw"] == expected_result["draw"], "Wrong value for draw"
-            # 5. assert result["guest"] == expected_result["guest"], "Wrong value for guest_win"
+        assert result == expected_result, f"Result wasn't what we expected... {result} != {expected_result}"
+        # This check incorperates all of the following:
+        # 0. assert len(result) == len(expected_result), "Result has wrong length"
+        # 1. assert len(set(result.keys()) - set(expected_result.keys())) == 0, "Unexpect result keys"
+        # 2. assert len(set(result.values()) - set(expected_result.values())) == 0, "Unexpected result values"
+        # 3. assert result["host"] == expected_result["host"], "Wrong value for host_win"
+        # 4. assert result["draw"] == expected_result["draw"], "Wrong value for draw"
+        # 5. assert result["guest"] == expected_result["guest"], "Wrong value for guest_win"
