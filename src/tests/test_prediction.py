85d121d2c45134a83253eec35525dd242419036f
Tim Fischer <t.fischer@student.uni-tuebingen.de>
2018-12-26 11:28:39 +0100

    fixed tests to work with new internal api
    After changing a lot of stuff around in !3
    and now in !4 some of the tests broke dew
    to the usage of old ("deprecated") internal api
    usage. It's bad when your own tests don't
    even work so I went ahead and fixed them
    here.
    
    Relates to #4 and #5

--- src/tests/test_prediction.py
+++ src/tests/test_prediction.py
@@ -21,19 +21,12 @@ class TestPrediction(unittest.TestCase):
         p = PoissonPredictor()
         with self.DB.get_session() as session:
             p.calculate_model(RangeSelector(), session)
-        result = {
-            key: round(val, 2)
-            for key, val in p.make_prediction(
-                "FC Bayern", "Borussia Dortmund"
-            ).items()
-        }
-        expected_result = {'host': 0.73, 'draw': 0.16, 'guest': 0.11}
+        result_obj = p.make_prediction("FC Bayern", "Borussia Dortmund")
 
-        assert result == expected_result, f"Result wasn't what we expected... {result} != {expected_result}"
-        # This check incorperates all of the following:
-        # 0. assert len(result) == len(expected_result), "Result has wrong length"
-        # 1. assert len(set(result.keys()) - set(expected_result.keys())) == 0, "Unexpect result keys"
-        # 2. assert len(set(result.values()) - set(expected_result.values())) == 0, "Unexpected result values"
-        # 3. assert result["host"] == expected_result["host"], "Wrong value for host_win"
-        # 4. assert result["draw"] == expected_result["draw"], "Wrong value for draw"
-        # 5. assert result["guest"] == expected_result["guest"], "Wrong value for guest_win"
+        assert all(
+            round(actual, 2) == expected
+            for actual, expected in zip(
+                (result_obj.host_win_propability, result_obj.draw_propability, result_obj.guest_win_propability),
+                (0.73, 0.16, 0.11)
+            )
+        ), f"Result wasn't what we expected... {result} != {expected_result}"
